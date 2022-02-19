42611beab4158a681320d741275d47642ae1f511
habatoo <habatoo@gmail.com>
2022-02-20 01:04:19 +0700

    last version

--- src/main/java/numbers/Main.java
+++ src/main/java/numbers/Main.java
@@ -1,131 +1,76 @@
 package numbers;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
 import java.util.StringJoiner;
 
-public class Main {
-    static Scanner scanner = new Scanner(System.in);
-    static long number;
-    static long count;
-    static String property;
-    static String[] propertyArray = new String[]{
-            "BUZZ",
-            "DUCK",
-            "PALINDROMIC",
-            "GAPFUL",
-            "SPY",
-            "EVEN",
-            "ODD"};
-    static String[] requestParts;
+import static java.util.Arrays.binarySearch;
+
+public final class Main {
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        readNumber();
+        showIntro();
+        run();
     }
 
-    public static void readNumber() {
-        showIntro();
-
+    private static void run() {
         while (true) {
-            System.out.println("Enter a request:");
-            int scenario = chooseScenario();
-            if (scenario == 1) {
-                int result = isNatural(requestParts[0]);
-                if (result == -1) {
-                    System.out.println("The first parameter should be a natural number or zero.");
-                } else if (result == 0) {
-                    System.out.println("Goodbye!");
-                    break;
-                } else {
-                    number = Long.parseLong(requestParts[0], 10);
-                    showFirstScenarioResult();
-                }
-            } else if (scenario == 2) {
-                int resultFirst = isNatural(requestParts[0]);
-                int resultSecond = isNatural(requestParts[1]);
-                if (resultFirst == 0) {
-                    System.out.println("Goodbye! ");
-                    break;
-                } else if (resultFirst == -1){
-                    System.out.println("The first parameter should be a natural number or zero.");
-                } else if (resultSecond == -1){
-                    System.out.println("The second parameter should be a natural number.");
-                } else {
-                    number = Long.parseLong(requestParts[0], 10);
-                    count = Long.parseLong(requestParts[1], 10);
-                    for (int i = 0; i < count; i++) {
-                        showSecondScenarioResult();
-                        number++;
-                    }
-                }
-            } else if (scenario == 3) {
-                int resultFirst = isNatural(requestParts[0]);
-                int resultSecond = isNatural(requestParts[1]);
-                boolean resultThird = isInProperty(requestParts[2]);
+            String[] request = readRequest();
 
-                if (resultFirst == 0) {
-                    System.out.println("Goodbye! ");
-                    break;
-                } else if (resultFirst == -1){
-                    System.out.println("The first parameter should be a natural number or zero.");
-                } else if (resultSecond == -1){
-                    System.out.println("The second parameter should be a natural number.");
-                } else if (!resultThird){
-                    System.out.printf("The property [%s]  is wrong.\n", requestParts[2].toUpperCase());
-                    System.out.println("Available properties: [BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, EVEN, ODD]");
-                } else {
-                    number = Long.parseLong(requestParts[0], 10);
-                    count = Long.parseLong(requestParts[1], 10);
-                    property = requestParts[2];
-                    while (count > 0) {
-                        switch (property) {
-                            case ("EVEN"):
-                                if (!isOddOrEven()) {
-                                    showThirdScenarioResult();
-                                    count--;
-                                }
-                                break;
-                            case ("ODD"):
-                                if (isOddOrEven()) {
-                                    showThirdScenarioResult();
-                                    count--;
-                                }
-                                break;
-                            case ("SPY"):
-                                if (isSpy()) {
-                                    showThirdScenarioResult();
-                                    count--;
-                                }
-                                break;
-                            case ("GAPFUL"):
-                                if (isGapful()) {
-                                    showThirdScenarioResult();
-                                    count--;
-                                }
-                                break;
-                            case ("PALINDROMIC"):
-                                if (isPalindromic()) {
-                                    showThirdScenarioResult();
-                                    count--;
-                                }
-                                break;
-                            case ("DUCK"):
-                                if (isDuck()) {
-                                    showThirdScenarioResult();
-                                    count--;
-                                }
-                                break;
-                            case ("BUZZ"):
-                                if (isBuzz()) {
-                                    showThirdScenarioResult();
-                                    count--;
-                                }
-                                break;
-                        }
-                        number++;
-                    }
+            if ("0".equals(request[0])) {
+                System.out.println("Goodbye!");
+                break;
+            }
+            if (notNatural(request[0])) {
+                System.out.println("The first parameter should be a natural number or zero.");
+                continue;
+            }
+
+            NaturalNumber naturalNumber = new NaturalNumber(request[0]);
+
+            if (request.length == 1) {
+                naturalNumber.printCard();
+                continue;
+            }
+            if (notNatural(request[1])) {
+                System.out.println("The second parameter should be a natural number.");
+                continue;
+            }
+            int count = Integer.parseInt(request[1]);
+            String[] query = request.length == 3 ? request[2].split(" ") : new String[0];
+            String wrong = NaturalNumber.getWrongProperties(query);
+            if (wrong.length() > 0) {
+                System.out.printf(wrong.contains(", ") ?
+                        "The properties %s are wrong" : "The property %s is wrong", wrong);
+                System.out.println("Available properties: ");
+                System.out.println(Arrays.toString(NaturalNumber.PROPERTIES));
+                continue;
+            }
+
+            String mutuallyExclusivePairs = NaturalNumber.getMutuallyExclusive(query);
+
+            if (mutuallyExclusivePairs.length() > 0) {
+                System.out.print("The request contains mutually exclusive properties: ");
+                System.out.println(mutuallyExclusivePairs);
+                System.out.println("There are no numbers with these properties.");
+                continue;
+            }
+
+            while (count > 0) {
+                if (naturalNumber.hasProperties(query)) {
+                    naturalNumber.printLine();
+                    count--;
                 }
+                naturalNumber.increment();
             }
         }
+    }
+
+    private static String[] readRequest() {
+        System.out.println("Enter a request:");
+        return scanner.nextLine().toLowerCase().split(" ", 3);
     }
 
     private static void showIntro() {
@@ -134,147 +79,208 @@ public class Main {
         System.out.println("- enter a natural number to know its properties;");
         System.out.println("- enter two natural numbers to obtain the properties of the list:");
         System.out.println("  * the first parameter represents a starting number;");
-        System.out.println("  * the second parameter shows how many consecutive numbers are to be printed;");
-        System.out.println("- two natural numbers and a property to search for;");
+        System.out.println("  * the second parameter shows how many consecutive numbers are to be processed;");
+        System.out.println("- two natural numbers and properties to search for;");
+        System.out.println("- a property preceded by minus must not be present in numbers;");
         System.out.println("- separate the parameters with one space;");
         System.out.println("- enter 0 to exit.\n");
     }
 
-    private static void showFirstScenarioResult() {
-        System.out.printf("Properties of %d\n", number);
-        System.out.printf("\tbuzz:\t%b\n", isBuzz());
-        System.out.printf("\tduck:\t%b\n", isDuck());
-        System.out.printf("\tpalindromic:\t%b\n", isPalindromic());
-        System.out.printf("\tgapful:\t%b\n", isGapful());
-        System.out.printf("\tspy:\t%b\n", isSpy());
-        System.out.printf("\teven:\t%b\n", !isOddOrEven());
-        System.out.printf("\todd:\t%b\n", isOddOrEven());
-    }
-
-    private static void showSecondScenarioResult() {
-        StringJoiner joiner = new StringJoiner(", ");
-        if (isOddOrEven()) joiner.add("even");
-        if (!isOddOrEven()) joiner.add("odd");
-        if (isBuzz()) joiner.add("buzz");
-        if (isDuck()) joiner.add("duck");
-        if (isSpy()) joiner.add("spy");
-        if (isPalindromic()) joiner.add("palindromic");
-        if (isGapful()) joiner.add("gapful");
-        String result = joiner.toString().trim();
-        System.out.printf("\t%d is %s\n", number, result);
-    }
-
-    private static void showThirdScenarioResult() {
-        StringJoiner joiner = new StringJoiner(", ");
-        if (isOddOrEven()) joiner.add("even");
-        if (!isOddOrEven()) joiner.add("odd");
-        if (isBuzz()) joiner.add("buzz");
-        if (isDuck()) joiner.add("duck");
-        if (isSpy()) joiner.add("spy");
-        if (isPalindromic()) joiner.add("palindromic");
-        if (isGapful()) joiner.add("gapful");
-        String result = joiner.toString().trim();
-
-        System.out.printf("\t%d is %s\n", number, result);
-    }
-
-    private static int chooseScenario() {
-        int scenario = 1;
-        requestParts = scanner.nextLine().split(" ");
-        if (requestParts.length == 2) {
-            scenario = 2;
-        } else if (requestParts.length == 3) {
-            scenario = 3;
-        }
-        return scenario;
-    }
-
-    private static boolean isInProperty(String string) {
-        return Arrays.stream(propertyArray).anyMatch(string.toUpperCase()::equals);
-    }
-
-    private static int isNatural(String string) {
-        int natural = 1;
+    static boolean notNatural(String string) {
+        boolean notNatural = false;
         try {
             Long number = Long.parseLong(string, 10);
             if (number < 0) {
-                natural = -1;
-            } else if (number == 0) {
-                natural = 0;
+                notNatural = true;
             }
         } catch (Exception e) {
-            natural = -1;
+            notNatural = true;
         }
-        return natural;
+        return notNatural;
     }
 
-    private static boolean isOddOrEven() {
-        boolean odd = true;
-        if (number % 2 == 0) {
-            odd = false;
+}
+
+class NaturalNumber {
+    static final String[] PROPERTIES = new String[]{
+            "even",
+            "odd",
+            "buzz",
+            "duck",
+            "palindromic",
+            "gapful",
+            "spy",
+            "square",
+            "sunny",
+            "jumping",
+            "happy",
+            "sad"
+    };
+
+    private static final String[][] EXCLUSIVE = new String[][]{
+            {"even", "odd"}, {"spy", "duck"}, {"sunny", "square"},
+            {"happy", "sad"}, {"-even", "-odd"}, {"-happy", "-sad"}
+    };
+
+    public static final String[][] MUTUALLY_EXCLUSIVE = new String[EXCLUSIVE.length + PROPERTIES.length][];
+
+    static {
+        Arrays.sort(PROPERTIES);
+        int index = 0;
+        for (String[] pair : EXCLUSIVE) {
+            MUTUALLY_EXCLUSIVE[index++] = pair;
         }
-        return odd;
+        for (String property : PROPERTIES) {
+            MUTUALLY_EXCLUSIVE[index++] = new String[]{property, "-" + property};
+        }
     }
 
-    private static boolean isBuzz() {
-        boolean buzz = false;
-        if (number >= 7 && number % 7 == 0 || number % 10 == 7) {
-            buzz = true;
-        }
-        return buzz;
+    private String digits;
+    private long number;
+
+    NaturalNumber(String value) {
+        digits = value;
+        number = Long.parseLong(value);
     }
 
-    private static boolean isDuck() {
-        boolean duck = true;
-        String string = Long.toString(number);
-        if (!string.substring(1).contains("0")) {
-            duck = false;
-        }
-        return duck;
+    NaturalNumber(long value) {
+        digits = String.valueOf(value);
+        number = value;
     }
 
-    private static boolean isPalindromic(){
-        boolean palindromic = true;
-        String string = Long.toString(number);
-        String stringRev = new StringBuilder(string).reverse().toString();
-        if (!string.equals(stringRev)) {
-            palindromic = false;
-        }
-        return palindromic;
+    static boolean isWrong(String property) {
+        return binarySearch(PROPERTIES, property) < 0;
     }
 
-    private static boolean isGapful(){
-        boolean gapful = true;
-        if (number < 100) {
-            gapful = false;
-        } else {
-            long first = number;
-            while(first > 9) {
-                first /= 10;
+    static String getWrongProperties(String[] query) {
+        StringJoiner wrong = new StringJoiner(", ");
+        for (String property : query) {
+            String name = property.charAt(0) == '-' ? property.substring(1) : property;
+            if (NaturalNumber.isWrong(name)) {
+                wrong.add(property);
             }
-            long last = number % 10;
-            long div = first * 10 + last;
-            gapful = number % div == 0 ? true : false;
         }
-        return gapful;
+        return wrong.toString();
     }
 
-    private static boolean isSpy(){
-        boolean spy = true;
-        long n = number;
+    static String getMutuallyExclusive(String[] query) {
+        Arrays.sort(query);
+        StringJoiner me = new StringJoiner(", ");
+        for (String[] pair : MUTUALLY_EXCLUSIVE) {
+            boolean containsPair = binarySearch(query, pair[0]) >= 0 && binarySearch(query, pair[1]) >= 0;
+            if (containsPair) {
+                me.add(pair[0] + " and " + pair[1]);
+            }
+        }
+        return me.toString();
+    }
+
+    void printCard() {
+        System.out.printf("Properties of %,d%n", number);
+        for (String property : PROPERTIES) {
+            boolean hasProperty = test(property);
+            System.out.printf("%12s: %s%n", property, hasProperty);
+        }
+    }
+
+    void printLine() {
+        StringJoiner properties = new StringJoiner(", ");
+        for (String property : PROPERTIES) {
+            if (test(property)) {
+                properties.add(property);
+            }
+        }
+        System.out.printf("%,12d is %s%n", number, properties);
+    }
+
+    private boolean test(String property) {
+        switch (property) {
+            case "even":
+                return number % 2 == 0;
+            case "odd":
+                return number % 2 != 0;
+            case "buzz":
+                return number % 7 == 0 || number % 10 == 7;
+            case "duck":
+                return digits.indexOf('0') != -1;
+            case "palindromic":
+                return new StringBuilder(digits).reverse().toString().equals(digits);
+            case "gapful":
+                long divider = (digits.charAt(0) - '0') * 10 + number % 10;
+                return number >= 100 && number % divider == 0;
+            case "spy":
+                return isSpy();
+            case "square":
+                return (long) Math.pow((long) Math.sqrt(number), 2) == number;
+            case "sunny":
+                return new NaturalNumber(number + 1).test("square");
+            case "jumping":
+                return isJumping();
+            case "happy":
+                return isHappy();
+            case "sad":
+                return !isHappy();
+        }
+        return false;
+    }
+
+    void increment() {
+        number++;
+        digits = String.valueOf(number);
+    }
+
+    boolean hasProperties(String[] query) {
+        for (String property : query) {
+            boolean isNegative = property.charAt(0) == '-';
+            if (isNegative ? test(property.substring(1)) : !test(property)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isSpy() {
         long sum = 0L;
         long mul = 1L;
-
-        if (number >= 10) {
-            while(n > 0) {
-                sum += n % 10;
-                mul *= n % 10;
-                n /= 10;
-            }
-            spy = sum == mul ? true : false;
+        for (long i = number; i > 0; i /= 10) {
+            mul *= i % 10;
+            sum += i % 10;
         }
+        return mul == sum;
+    }
 
-        return spy;
+    private boolean isJumping() {
+        boolean isJumping = true;
+        for (long numPrev = number % 10, next = number / 10; next > 0; next /= 10) {
+            long numNext = next % 10;
+            if (Math.abs(numPrev - numNext) != 1) {
+                isJumping = false;
+                break;
+            }
+            numPrev = numNext;
+        }
+        return isJumping;
+    }
+
+    private boolean isHappy() {
+        boolean isHappy = false;
+        HashSet sequence = new HashSet<Long>();
+        for (long i = number; !sequence.contains(i); i = happyNext(i)) {
+            if (i == 1) {
+                isHappy = true;
+            }
+            sequence.add(i);
+        }
+        return isHappy;
+    }
+
+    private long happyNext(long number) {
+        long result = 0;
+        for (long i = number; i > 0; i /= 10) {
+            int digit = (int) (i % 10);
+            result += digit * digit;
+        }
+        return result;
     }
 
 }
